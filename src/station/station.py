a585b28800b4db9801ee84c2aa625169d88b4a63
grishman81 <grishman1981@gmail.com>
2026-08-07 09:29:30 +0500

    feat: connect station with playlist engine

--- src/station/station.py
+++ src/station/station.py
@@ -3,11 +3,12 @@ ArtistRadio Engine
 Station
 """
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import Optional
 
-from library.library import Library
-from library.models import Track
+from ..library.library import Library
+from ..library.models import Track
+from ..playlist.engine import PlaylistEngine
 
 
 @dataclass
@@ -18,24 +19,22 @@ class Station:
 
     name: str
     artist: str
+    playlist: PlaylistEngine
 
     bitrate: int = 320
 
     current_track: Optional[Track] = None
 
-    history: list[Track] = field(default_factory=list)
-
     def next_track(self, library: Library) -> Optional[Track]:
         """
-        Пока заглушка.
-
-        В следующем пакете будет выбирать случайный трек
-        исполнителя и следить за историей.
+        Выбирает следующий трек станции.
         """
-        return None
 
-    def add_history(self, track: Track) -> None:
-        self.history.append(track)
+        tracks = library.get_tracks(self.artist)
 
-        if len(self.history) > 100:
-            self.history.pop(0)
+        if not tracks:
+            return None
+
+        self.current_track = self.playlist.next_track(tracks)
+
+        return self.current_track
