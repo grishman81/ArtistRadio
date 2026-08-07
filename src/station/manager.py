c96b29f228d34af719fc7a0ae74720592b1cac1d
grishman81 <grishman1981@gmail.com>
2026-08-07 10:35:55 +0500

    feat: load stations from config

--- src/station/manager.py
+++ src/station/manager.py
@@ -3,7 +3,10 @@ ArtistRadio Engine
 Station Manager
 """
 
+from pathlib import Path
+
 from .station import Station
+from .loader import StationLoader
 
 
 class StationManager:
@@ -11,8 +14,11 @@ class StationManager:
     Управляет набором радиостанций.
     """
 
-    def __init__(self):
+    def __init__(self, library=None, playlist=None):
         self._stations: dict[str, Station] = {}
+
+        self.library = library
+        self.playlist = playlist
 
     def add(self, station: Station) -> None:
         self._stations[station.name] = station
@@ -25,6 +31,21 @@ class StationManager:
 
     def all(self) -> list[Station]:
         return list(self._stations.values())
+
+    def load_from_file(self, path: Path) -> None:
+        """
+        Загружает станции из JSON файла.
+        """
+
+        loader = StationLoader(
+            self.library,
+            self.playlist,
+        )
+
+        stations = loader.load(path)
+
+        for station in stations:
+            self.add(station)
 
     @property
     def count(self) -> int:
