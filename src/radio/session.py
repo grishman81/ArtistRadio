5e2f46236ca781c9d972fed5e9c1545e8341e914
grishman81 <grishman1981@gmail.com>
2026-08-07 23:31:42 +0500

    feat: save active playlist mode in radio session

--- src/radio/session.py
+++ src/radio/session.py
@@ -20,10 +20,6 @@ from src.library.models import Track
 
 class RadioSession:
 
-    """
-    Управляет эфирной сессией.
-    """
-
     def __init__(
         self,
         radio: RadioEngine,
@@ -31,30 +27,38 @@ class RadioSession:
         player: AudioPlayer,
         history: PlaybackHistory,
     ):
+
         self.radio = radio
         self.storage = storage
         self.player = player
         self.history = history
 
         self.state = RadioState(
-            station=radio.station.name
+            station=radio.station.name,
+            mode=radio.station.get_mode().value,
         )
 
         self.save()
 
+
     def save(self):
+
         self.storage.save(
             self.state
         )
 
+
     def start(self):
+
         self.state.running = True
 
         self.radio.start()
 
         self.save()
 
+
     def stop(self):
+
         self.state.running = False
 
         self.player.stop()
@@ -63,12 +67,16 @@ class RadioSession:
 
         self.save()
 
+
     def play_next(self) -> Optional[Track]:
 
         if not self.state.running:
+
             return None
 
+
         track = self.radio.next()
+
 
         if track:
 
@@ -86,19 +94,26 @@ class RadioSession:
 
             self.save()
 
+
         return track
+
+
 
     def play_history_item(
         self,
         index: int,
     ) -> Optional[Track]:
 
+
         item = self.history.get(
             index
         )
 
+
         if item is None:
+
             return None
+
 
         track = Track(
             artist=item["artist"],
@@ -110,37 +125,55 @@ class RadioSession:
             ),
         )
 
+
         self.player.play(
             track.path
         )
+
 
         self.state.track = str(
             track.path
         )
 
+
         self.save()
 
+
         return track
+
+
 
     def resume_playback(self):
 
         return self.play_next()
+
+
 
     def switch_station(
         self,
         station: Station,
     ):
 
+
         self.player.stop()
+
 
         self.radio = RadioEngine(
             station
         )
 
+
         self.state.station = (
             station.name
         )
 
+
+        self.state.mode = (
+            station.get_mode().value
+        )
+
+
         self.state.track = None
+
 
         self.save()
